bf29db553af8b6c19eaef0edfbc400d548e54c50
Maniac <52683414+Maniacsan@users.noreply.github.com>
2025-07-26 18:50:11 -0400

    Update torrra.rb

--- torrra.rb
+++ torrra.rb
@@ -1,15 +1,15 @@
 class Torrra < Formula
-  version "1.2.0"
-  sha256 "b4615d99763b9f4aeb1f3066419111f058b5f1fb1ca64e5f1044672d60a9df6f"
+  version "1.2.2"
+  sha256 "ada9a35ac9aa36bc8fa289c90561d7cc9a1fd71da7e6d2eecfb215bfa3b894e4"
 
-  url "https://github.com/stabldev/torrra/releases/download/v1.2.0/torrra_1.2.0_macos_arm64"
+  url "https://github.com/stabldev/torrra/releases/download/v1.2.2/torrra_1.2.2_macos_arm64"
   desc "A Python tool that lets you find and download torrents without leaving your CLI."
   name "Torrra"
   homepage "https://github.com/stabldev/torrra"
   
 
   def install
-    bin.install "torrra_1.2.0_macos_arm64" => "torrra"
+    bin.install "torrra_1.2.2_macos_arm64" => "torrra"
   end
 
   test do
