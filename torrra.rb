7d01f23f40b170db5c97d66953a87fe65dcd5e23
Maniac <52683414+Maniacsan@users.noreply.github.com>
2025-07-26 08:18:20 -0400

    Update torrra.rb

--- torrra.rb
+++ torrra.rb
@@ -1,11 +1,11 @@
 class Torrra < Formula
   desc "A Python tool that lets you find and download torrents without leaving your CLI."
   homepage "https://github.com/stabldev/torrra" # Replace with actual URL
-  url "https://github.com/stabldev/torrra/releases/download/v1.1.2/torrra_1.1.2_macos_arm64" # Update URL
-  sha256 "67b8ea24e8e8f641bc9cb07c451511e40f7a3f4d471aa5d3122c424274d8df21" # Replace with the actual SHA256
+  url "https://github.com/stabldev/torrra/releases/download/v1.2.0/torrra_1.2.0_macos_arm64" # Update URL
+  sha256 "b4615d99763b9f4aeb1f3066419111f058b5f1fb1ca64e5f1044672d60a9df6f" # Replace with the actual SHA256
 
   def install
-    bin.install "torrra_1.1.2_macos_arm64" => "torrra"# Install the executable
+    bin.install "torrra_1.2.0_macos_arm64" => "torrra"# Install the executable
   end
 
   test do
